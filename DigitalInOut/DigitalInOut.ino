9c2288b882adb046490b19350d967297260b23a1
Daniel Molin <dmol@kth.se>
2014-01-14 12:59:04 +0100

    Reads from analog sensors, sends over Serial

--- DigitalInOut/DigitalInOut.ino
+++ DigitalInOut/DigitalInOut.ino
@@ -1,8 +1,18 @@
 const int dPins[]  = {
   8,  // tilt sensor 1A
-  9,  // tilt sensor 1B
+  9  // tilt sensor 1B
 };
+
+const int bPins[] = {
+  A0,  // thumb
+  A1,  // index
+  A2,  // middle
+  A3,  // ring
+  A4  // pinky
+};
+
 int dNoPins = sizeof(dPins)/sizeof(int);
+int bNoPins = sizeof(bPins)/sizeof(int);
 int SAMPLES_PER_SECOND = 20;
 
 int buttonPin = 12;
@@ -14,25 +24,39 @@ void setup() {
   for(int i = 0; i < dNoPins; i++){
     pinMode(dPins[i], INPUT); 
   }
+  for(int i = 0; i < bNoPins; i++){
+    pinMode(bPins[i], INPUT); 
+    digitalWrite(bPins[i], HIGH);
+  }
 }
 
 void loop() {
-  // read and send digital value
-  for(int i = 0; i < dNoPins; i++){
-    sendInt16AsBytes(digitalRead(dPins[i]));
-    digitalWrite(dPins[i], LOW);
+  //  // read and send digital value
+  //  for(int i = 0; i < dNoPins; i++){
+  //    sendInt16AsBytes(digitalRead(dPins[i]));
+  //    digitalWrite(dPins[i], LOW);
+  //  }
+  //
+  //  digitalWrite(buttonPin, HIGH);
+  //
+  //  if(digitalRead(buttonPin)){
+  //    // Combine the tilt sensor values to one int
+  //    //int tiltValue = (int)(digitalRead(dPins[0]) << 1 | digitalRead(dPins[1]));
+  //    //sendInt16AsBytes(tiltValue);
+  //
+  //    // End transmission with a linebreak (sortof)
+  //    Serial.write(10);
+  //  }
+
+  for(int i = 0; i < bNoPins; i++){
+    int value = map(analogRead(bPins[i]), 200, 700, 0, 100);
+        //Serial.print(value);
+    sendInt16AsBytes(value);
+
   }
-
-  digitalWrite(buttonPin, HIGH);
-
-  if(digitalRead(buttonPin)){
-    // Combine the tilt sensor values to one int
-    //int tiltValue = (int)(digitalRead(dPins[0]) << 1 | digitalRead(dPins[1]));
-    //sendInt16AsBytes(tiltValue);
-
-    // End transmission with a linebreak (sortof)
-    Serial.write(10);
-  }
+ // Serial.println();
+  // End transmission with a linebreak (sortof)
+  Serial.write(10);
   // wait 
   // for the analog-to-digital converter to settle
   // after the last reading:
@@ -46,6 +70,8 @@ void sendInt16AsBytes(int data){
   Serial.write(highByte(data));
   Serial.write(lowByte(data));
 }
+
+
 
 
 
